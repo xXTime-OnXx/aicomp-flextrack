d96daff4f066d02c4e311713613444e996ef40b5
timon <timon.schmid@bluewin.ch>
2025-12-10 16:58:49 +0100

    add weighted loss

--- src/utils/losses.py
+++ src/utils/losses.py
@@ -44,3 +44,16 @@ class PenalizedMSELoss(nn.Module):
         total_loss = base_loss + penalty_term
         
         return total_loss, base_loss, penalty_term
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2):
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+    
+    def forward(self, inputs, targets):
+        ce_loss = F.cross_entropy(inputs, targets, reduction='none')
+        pt = torch.exp(-ce_loss)
+        focal_loss = self.alpha * (1-pt)**self.gamma * ce_loss
+        return focal_loss.mean()
